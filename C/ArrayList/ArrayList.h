39bd99a559c9b6d517d0ead4c28a1608ef7be843
jonathanveedot <jonathanveedot@gmail.com>
2017-05-10 19:29:13 -0400

    Update ArrayList.h

--- C/ArrayList/ArrayList.h
+++ C/ArrayList/ArrayList.h
@@ -2,63 +2,46 @@
 #define __ARRAY_LIST_H
 
 // Default capacity for new ArrayLists
-
 #define DEFAULT_INIT_LEN 10
-
 
 typedef struct ArrayList
 {
 	// We will store an array of strings (i.e., an array of char arrays)
-	
-char **array;
+	char **array;
 
 	// Size of list (i.e., number of elements that have been added to the array)
+	int size;
 	
-int size;
-
-		
-int capacity; // Length of the array (i.e., the array's current maximum capacity)
-
-
+	// Length of the array (i.e., the array's current maximum capacity)
+	int capacity; 
 } 
 
 ArrayList;
 
 
-// Functional Prototypes
+// Function Prototypes
 
 ArrayList 
 *createArrayList(int length);
 
-
 ArrayList *destroyArrayList(ArrayList *list);
-
 
 ArrayList *expandArrayList(ArrayList *list, int length);
 
-
 ArrayList *trimArrayList(ArrayList *list);
-
 
 char *put(ArrayList *list, char *str);
 
-
 char *get(ArrayList *list, int index);
-
 
 char *set(ArrayList *list, int index, char *str);
 
-
 char *insertElement(ArrayList *list, int index, char *str);
-
 
 int removeElement(ArrayList *list, int index);
 
-
 int getSize(ArrayList *list);
 
-
 void printArrayList(ArrayList *list);
-
 
 #endif
