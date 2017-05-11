2326a7574b1190d0077cf50a36935ac13147362b
jonathanveedot <jonathanveedot@gmail.com>
2017-05-11 12:33:02 -0400

    Update BinarySearchTree.java

--- Java/BinarySearchTree/BinarySearchTree.java
+++ Java/BinarySearchTree/BinarySearchTree.java
@@ -1,11 +1,11 @@
 /**
  * BinarySearchTree.java
- *
  * This recitation demonstrates the fundamentals of binary search trees (BSTs).
  * In this implementation, each node in the BST tracks its size and rank within
  * the tree (the lowest value is ranked #1). Insertion is constrained by some 
  * value k, representing the minimum acceptable distance from other nodes in the
  * tree. 
+ * Dependencies: BST.java
  */
 package binarysearchtree;
 
